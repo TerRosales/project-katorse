430ca8b1580c09ac206e94a324a39f0caef1fbd9
Terrence Rosales <118107221+Romo3@users.noreply.github.com>
2023-11-04 13:03:23 -0400

    map set up

--- js/main.js
+++ js/main.js
@@ -1,114 +1,83 @@
-// function initMap() {
-            
-//     const coordinates = { lat: 40.689, lng: -74.044 };
 
-//     const map = new google.maps.Map(document.getElementById("map"), {
-//         zoom: 13,
-//         center: coordinates,
-//     });
-
-//     const marker = new google.maps.Marker({
-//         position: coordinates,
-//         map: map,
-//     });
-    
-
-//     const circle = new google.maps.Circle({
-//         strokeColor: "blue",
-//         strokeOpacity: 0.8,
-//         strokeWeight: 2,
-//         fillColor: "lightseagreen",
-//         fillOpacity: 0.5,
-//         map,
-//         center: coordinates,
-//         radius: 600,
-//         });
-    
-//     const rectangle = new google.maps.Rectangle({
-//         strokeColor: "maroon",
-//         strokeOpacity: 0.8,
-//         strokeWeight: 2,
-//         fillColor: "coral",
-//         fillOpacity: 0.1,
-//         map,
-//         bounds: {
-//             north: 40.699, // Adjust the latitude values to fit your desired bounds
-//             south: 40.679,
-//             east: -74.024, // Adjust the longitude values to fit your desired bounds
-//             west: -74.064
-//         }
-//     });
-    
-    
-// };
-
-// var map;
-// var service;
-// var infowindow;
-
-// function initMap() {
-//   var sydney = new google.maps.LatLng(-33.867, 151.195);
-
-//   infowindow = new google.maps.InfoWindow();
-
-//   map = new google.maps.Map(
-//       document.getElementById('map'), {center: sydney, zoom: 15});
-
-//   var request = {
-//     query: 'Museum of Contemporary Art Australia',
-//     fields: ['name', 'geometry'],
-//   };
-
-//   var service = new google.maps.places.PlacesService(map);
-
-//   service.findPlaceFromQuery(request, function(results, status) {
-//     if (status === google.maps.places.PlacesServiceStatus.OK) {
-//       for (var i = 0; i < results.length; i++) {
-//         createMarker(results[i]);
-//       }
-//       map.setCenter(results[0].geometry.location);
-//     }
-//   });
-// }
-
-
-var map;
-var service;
-var infowindow;
 
 function initMap() {
-  var sydney = new google.maps.LatLng(-33.867, 151.195);
+  const coordinates = { lat: 41.113770, lng: -85.151772 };
 
-  infowindow = new google.maps.InfoWindow();
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 13,
+    center: coordinates,
+  });
 
-  map = new google.maps.Map(
-      document.getElementById('map'), {center: sydney, zoom: 15});
+  // Create a default marker for your coordinates
+  const marker = new google.maps.Marker({
+    position: coordinates,
+    map: map, // Assign the map
+  });
 
-  var request = {
-    query: 'Museum of Contemporary Art Australia',
-    fields: ['name', 'geometry'],
+  let openInfoWindow = null;
+
+  const service = new google.maps.places.PlacesService(map);
+
+  const request = {
+    location: coordinates,
+    radius: 12000,
+    type: ["restaurant", "gas_station", "lodging"],
   };
 
-  var service = new google.maps.places.PlacesService(map);
-
-  service.findPlaceFromQuery(request, function(results, status) {
+  service.nearbySearch(request, function (results, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
+      for (let i = 0; i < results.length; i++) {
         createMarker(results[i]);
       }
-      map.setCenter(results[0].geometry.location);
     }
   });
+
+  function createMarker(place) {
+    const marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location,
+      title: place.name,
+      icon: {
+        path: google.maps.SymbolPath.CIRCLE,
+        fillColor: "rgba(255, 0, 255, 0.8)",
+        fillOpacity: 1,
+        strokeColor: "rgba(0, 0, 200, 0.5)",
+        strokeOpacity: 1,
+        scale: 8,
+      },
+    });
+
+    const infowindow = new google.maps.InfoWindow({
+      maxWidth: "20rem",
+    });
+
+    google.maps.event.addListener(marker, "click", function () {
+      if (openInfoWindow) {
+        openInfoWindow.close();
+      }
+
+      const detailsRequest = {
+        placeId: place.place_id,
+        fields: ["name", "formatted_phone_number", "rating", "photos"],
+      };
+
+      const detailsService = new google.maps.places.PlacesService(map);
+
+      detailsService.getDetails(detailsRequest, function (placeDetails, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          const content = `
+            <h4>${placeDetails.name}</h4>
+            <p>Rating: ${placeDetails.rating}</p>
+            <p>Phone: ${placeDetails.formatted_phone_number}</p>
+            <img src="${placeDetails.photos[0].getUrl()}" alt="Place Photo" width="200" height="200">
+          `;
+
+          infowindow.setContent(content);
+          infowindow.open(map, marker);
+          openInfoWindow = infowindow;
+        }
+      });
+    });
+  }
 }
 
-function createMarker(place) {
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location,
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
-}
